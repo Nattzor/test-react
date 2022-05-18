74d5ef1e00529b919569d657a805c14fea316b72
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-05-18 13:43:49 +0200

    secretView begins

--- src/components/SecretView.js
+++ src/components/SecretView.js
@@ -1,9 +1,17 @@
+import { useState } from "react"
+
 function SecretView() {
+    const [login, setLogin] = useState(false)
+
+    function tryLogin(e){
+        console.log(e.target.value)
+    }
+
 return(<div>
 <form>
     <input type="text" placeholder="username"></input>
     <input type="password" placeholder="password"></input>
-    <button>Submit</button>
+    <button onClick={tryLogin(this.e)}>Submit</button>
 </form>
 </div>)
 }
